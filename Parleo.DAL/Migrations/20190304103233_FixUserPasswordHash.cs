8b9ea3a75c24e8a0dd44506310a1fa4be6bc8e2d
Tsaruk Alex <tsarukalex404@gmail.com>
2019-03-04 13:35:28 +0300

    fix(Migration) : change UserAuthModel
    run Update-Database to finish this commit

---
+++ Parleo.DAL/Migrations/20190304103233_FixUserPasswordHash.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parleo.DAL.Migrations
+{
+    public partial class FixUserPasswordHash : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "UserAuth");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "UserAuth",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "UserAuth",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "UserAuth");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "UserAuth");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "UserAuth",
+                nullable: true);
+        }
+    }
+}
