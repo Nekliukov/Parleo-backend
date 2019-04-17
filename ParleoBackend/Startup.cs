d529e9c92c9d105370e2ed46b144c187ed3af447
WildTiger404 <tsarukalex404@gmail.com>
2019-04-17 13:48:32 +0300

    fix(startup): fix configurations

--- ParleoBackend/Startup.cs
+++ ParleoBackend/Startup.cs
@@ -35,7 +35,7 @@ namespace ParleoBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
 
             services.AddSwaggerDocumentation();
 
@@ -77,7 +77,7 @@ namespace ParleoBackend
 
             services
                 .AddMvc()
-                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+                .SetCompatibilityVersion(CompatibilityVersion.Latest);
 
             services.AddTransient<IValidator<CreateOrUpdateEventViewModel>, CrateOrUpdateEventViewModelValidator>();
             services.AddTransient<IValidator<UserRegistrationViewModel>, UserRegistrationViewModelValidator>();
