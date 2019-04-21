ca29aae3fa8a088936fa3f094c12a1cb4e4e5724
Vadim Makarchik <makarchikvadim@gmail.com>
2019-04-21 13:49:15 +0300

    feat(prop): Add About property to User, add migration.

---
+++ Parleo.DAL/Migrations/20190421104759_AddAboutPropertyToUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parleo.DAL.Migrations
+{
+    public partial class AddAboutPropertyToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "About",
+                table: "User",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "About",
+                table: "User");
+        }
+    }
+}
