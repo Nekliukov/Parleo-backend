fc3d5aa0d824202c99b25a9ad1e11a1d4c6c2b2c
Kseniya <mikhailiuk.k@gmail.com>
2019-04-23 19:23:27 +0300

    add image to table

--- Parleo.DAL/Migrations/20190423161307_AddImageToEvent.Designer.cs
+++ Parleo.DAL/Migrations/20190423161307_AddImageToEvent.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Parleo.DAL;
 
 namespace Parleo.DAL.Migrations
 {
     [DbContext(typeof(AppContext))]
-    partial class AppContextModelSnapshot : ModelSnapshot
+    [Migration("20190423161307_AddImageToEvent")]
+    partial class AddImageToEvent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
