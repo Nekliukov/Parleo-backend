15774cb94cefd048169b887265ff5a5d8b3fff6c
Kseniya <mikhailiuk.k@gmail.com>
2019-04-25 01:32:07 +0300

    Add errors to response

--- ParleoBackend/Constants.cs
+++ ParleoBackend/Constants.cs
@@ -10,6 +10,8 @@
             public const string USER_NOT_FOUND = "User is not found";
             public const string EMAIL_NOT_FOUND = "User with such email does not exist";
             public const string WRONG_GUID_FORMAT = "Wrong GUID format";
+            public const string EXPIRED_TOKEN = "Token expired";
+            public const string NOT_VALID_TOKEN = "Token is not valid";
         }
     }
 }
