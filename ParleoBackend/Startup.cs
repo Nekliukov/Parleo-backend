6f563848d330b90ac74c1a93a4bda92bd1697319
Nekliukov, Roman <roman.nekliukov@thomsonreuters.com>
2019-04-25 22:20:40 +0300

    Fix location validation

--- ParleoBackend/Startup.cs
+++ ParleoBackend/Startup.cs
@@ -22,6 +22,7 @@ using ParleoBackend.Extensions;
 using ParleoBackend.Hubs;
 using ParleoBackend.Services;
 using ParleoBackend.Validators;
+using ParleoBackend.Validators.Common;
 using ParleoBackend.Validators.User;
 using ParleoBackend.ViewModels.Entities;
 
@@ -87,6 +88,7 @@ namespace ParleoBackend
             services.AddTransient<IValidator<UserRegistrationViewModel>, UserRegistrationViewModelValidator>();
             services.AddTransient<IValidator<UserLoginViewModel>, UserLoginViewModelValidator>();
             services.AddTransient<IValidator<UpdateUserViewModel>, UpdateUserViewModelValidator>();
+            services.AddTransient<IValidator<LocationViewModel>, LocationViewModelValidator>();
             ValidatorOptions.LanguageManager.Culture = new CultureInfo("en-GB");
             
             services.AddTransient<ClearExpiredTokenJob>();
---
+++ ParleoBackend/Validators/Common/LocationViewModelValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ParleoBackend.ViewModels.Entities;
+
+namespace ParleoBackend.Validators.Common
+{
+    public class LocationViewModelValidator : AbstractValidator<LocationViewModel>
+    {
+        public LocationViewModelValidator()
+        {
+            RuleFor(location => location.Latitude)
+                .InclusiveBetween(-90, 90).WithMessage(Constants.Errors.INVALID_LOCATION);
+            RuleFor(location => location.Longitude)
+                .InclusiveBetween(-180, 180).WithMessage(Constants.Errors.INVALID_LOCATION);
+        }
+    }
+}
